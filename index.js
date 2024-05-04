36a7afdde7ec805db1218a8344c9a897d9b33aab
Mark Salvatore <salvatore.mark@gmail.com>
2024-05-04 12:03:22 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,117 @@
+/**
+ * Dependencies
+ */
+const { join } = require("path");
+
+module.exports = {
+  assetsPath: join(__dirname, "plugin-assets"),
+  defaultValues: {
+    apiVersion: 3,
+    attributes: {
+      align: {
+        type: "string",
+        default: "full",
+      },
+      style: {
+        type: "object",
+        padding: {
+          top: "1em",
+          right: "1em",
+          bottom: "1em",
+          left: "1em",
+        },
+        default: {
+          color: {
+            background: "#F9F9F9",
+          },
+        },
+      },
+      textinput: {
+        type: "string",
+        default: "Write to me!",
+        source: "text",
+        selector: "div",
+      },
+      textalign: {
+        type: "string",
+        default: "center",
+      },
+      fontsize: {
+        type: "number",
+        default: 5,
+      },
+    },
+    author: "Mark Salvatore",
+    category: "widgets",
+    customBlockJSON: {},
+    customPackageJSON: {},
+    customScripts: {
+      script: "file:./script.js",
+    },
+    dashicon: "superhero",
+    description:
+      "A plugin created by the create-block tool using a custom external project template.",
+    domainPath: undefined,
+    editorScript: "file:./index.js",
+    editorStyle: "file:./index.css",
+    example: {
+      attributes: {
+        textinput: "Example text input",
+        fontsize: 8,
+      },
+    },
+    folderName: "./src",
+    license: "GPL-2.0-or-later",
+    licenseURI: "https://www.gnu.org/licenses/gpl-2.0.html",
+    namespace: "msalv",
+    npmDependencies: undefined,
+    npmDevDependencies: undefined,
+    plugin: true,
+    pluginURI: undefined,
+    render: "file:./render.php",
+    script: "file:./script.js",
+    slug: "create-block-template",
+    style: "file:./style-index.css",
+    supports: {
+      html: false,
+      align: true,
+      color: {
+        background: true,
+        text: true,
+      },
+      spacing: {
+        padding: true,
+        margin: false,
+      },
+    },
+    textdomain: "msalv-create-block-template",
+    title: "MSALV Create Block Template",
+    updateURI: undefined,
+    variantVars: { isDynamicVariant: true, isInteractiveVariant: false },
+    version: "0.1.0",
+    viewScript: "file:./view.js",
+    viewScriptModule: undefined,
+    viewStyle: undefined,
+    wpEnv: false,
+    wpScripts: true,
+    render: "file:./render.php",
+    customPackageJSON: {
+      prettier: "@wordpress/prettier-config",
+    },
+  },
+  variants: {
+    dynamic: {},
+    interactive: {
+      viewScriptModule: "file:./view.js",
+      customScripts: {
+        build: "wp-scripts build",
+        start: "wp-scripts start",
+      },
+      supports: {
+        interactive: true,
+      },
+    },
+  },
+  pluginTemplatesPath: join(__dirname, "files/plugin"),
+  blockTemplatesPath: join(__dirname, "files/block"),
+};

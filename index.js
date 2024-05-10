3bfcb7d8de312aa3d2e39cf0b36a0162a037b022
Mark Salvatore <salvatore.mark@gmail.com>
2024-05-10 15:49:17 -0400

    Updates to dynamic variant

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
 /**
  * Dependencies
  */
-const { join } = require("path");
+const { join } = require('path');
 
 module.exports = {
-  assetsPath: join(__dirname, "plugin-assets"),
+  assetsPath: join(__dirname, 'plugin-assets'),
   defaultValues: {
     apiVersion: 3,
     attributes: {
       align: {
-        type: "string",
-        default: "full",
+        type: 'string',
+        default: 'full',
       },
       style: {
-        type: "object",
+        type: 'object',
         padding: {
-          top: "1em",
-          right: "1em",
-          bottom: "1em",
-          left: "1em",
+          top: '1em',
+          right: '1em',
+          bottom: '1em',
+          left: '1em',
         },
         default: {
           color: {
-            background: "#F9F9F9",
+            background: '#F9F9F9',
           },
         },
       },
-      textinput: {
-        type: "string",
-        default: "Write to me!",
-        source: "text",
-        selector: "div",
+      textInput: {
+        type: 'string',
+        default: 'Write to me!',
+        source: 'text',
+        selector: 'div',
       },
-      textalign: {
-        type: "string",
-        default: "center",
+      textAlign: {
+        type: 'string',
+        default: 'center',
       },
-      fontsize: {
-        type: "number",
+      fontSize: {
+        type: 'number',
         default: 5,
       },
     },
-    author: "Mark Salvatore",
-    category: "widgets",
+    author: 'Mark Salvatore',
+    category: 'widgets',
     customBlockJSON: {},
     customPackageJSON: {},
     customScripts: {},
-    dashicon: "superhero",
+    dashicon: 'superhero',
     description:
-      "A new block created by the create-block tool using a custom external project template.",
+      'A new block created by the create-block tool using a custom external project template.',
     domainPath: undefined,
-    editorScript: "file:./index.js",
-    editorStyle: "file:./index.css",
+    editorScript: 'file:./index.js',
+    editorStyle: 'file:./index.css',
     example: {
       attributes: {
-        textinput: "Example text input",
-        fontsize: 8,
+        textInput: 'Example text input',
+        fontSize: 8,
       },
     },
-    folderName: "./src",
-    license: "GPL-2.0-or-later",
-    licenseURI: "https://www.gnu.org/licenses/gpl-2.0.html",
-    namespace: "msalv",
+    folderName: './src',
+    license: 'GPL-2.0-or-later',
+    licenseURI: 'https://www.gnu.org/licenses/gpl-2.0.html',
+    namespace: 'msalv',
     npmDependencies: undefined,
     npmDevDependencies: undefined,
     plugin: true,
     pluginURI: undefined,
-    render: "file:./render.php",
-    script: "file:./script.js",
-    slug: "new-block-project",
-    style: "file:./style-index.css",
+    render: 'file:./render.php',
+    script: 'file:./script.js',
+    slug: 'new-block-project',
+    style: 'file:./style-index.css',
     supports: {
       html: false,
       align: true,
@@ -82,31 +82,31 @@ module.exports = {
         margin: false,
       },
     },
-    textdomain: "new-block-project",
-    title: "New Block Project",
+    textdomain: 'new-block-project',
+    title: 'New Block Project',
     updateURI: undefined,
     variantVars: { isDynamicVariant: false, isInteractiveVariant: false },
-    version: "0.1.0",
+    version: '0.1.0',
     wpEnv: false,
     wpScripts: true,
-    render: "file:./render.php",
+    render: 'file:./render.php',
     customPackageJSON: {
-      prettier: "@wordpress/prettier-config",
+      prettier: '@wordpress/prettier-config',
     },
   },
   variants: {
     dynamic: {},
     interactive: {
-      viewScriptModule: "file:./view.js",
+      viewScriptModule: 'file:./view.js',
       customScripts: {
-        build: "wp-scripts build",
-        start: "wp-scripts start",
+        build: 'wp-scripts build',
+        start: 'wp-scripts start',
       },
       supports: {
         interactive: true,
       },
     },
   },
-  pluginTemplatesPath: join(__dirname, "files/plugin"),
-  blockTemplatesPath: join(__dirname, "files/block"),
+  pluginTemplatesPath: join(__dirname, 'files/plugin'),
+  blockTemplatesPath: join(__dirname, 'files/block'),
 };
